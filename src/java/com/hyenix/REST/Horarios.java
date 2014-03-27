9dac6b4e9fdacfd3b7770f8b78d6c0af11e7cc12
Santiago <sslkeyregit@gmail.com>
2014-03-26 22:56:24 -0600

    (Ya casi esta) Web Service Registro Horarios
    Algo del registrar horarios

--- src/java/com/hyenix/REST/Horarios.java
+++ src/java/com/hyenix/REST/Horarios.java
@@ -8,7 +8,6 @@ package com.hyenix.REST;
 
 import org.json.*;
 import java.sql.*;
-import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -27,7 +26,75 @@ public class Horarios {
    @POST
    @Consumes(MediaType.APPLICATION_JSON)
    @Produces(MediaType.APPLICATION_JSON)
-   public String RegistrarHorario(){
+   public String RegistrarHorario(String srt){
+       JSONObject horario=new JSONObject(srt);
+       JSONObject rtnStmt=new JSONObject();
+       try
+        {
+            try{
+               Class.forName("com.mysql-jdbc.Driver");
+            }
+            catch(ClassNotFoundException cnfEx){
+                rtnStmt.put("Registro", false);
+                rtnStmt.put("Mensaje","Driver no encontrado");
+                return rtnStmt.toString();
+            }
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/ENCOM","root","n0m3l0");
+            Statement stmt=conn.createStatement();
+            /*
+                Registro de Materia
+            */
+            int Profesor=horario.getInt("Profesor");
+            char Type_Schedule=horario.getString("Type_Schedule").charAt(0);
+            JSONArray materias=horario.getJSONArray("Materia");
+            for(int i=0;i<materias.length();i++){
+                JSONObject snt=materias.getJSONObject(i);
+                String nombre_materia=snt.getString("Nombre");
+                JSONArray grupo=snt.getJSONArray("Horario");
+                for(int j=0;i<snt.length();j++){
+                    JSONObject currentGroup=grupo.getJSONObject(j);
+                    String cntGp=currentGroup.getString("Grupo");
+                    if((currentGroup.getJSONObject("Lunes").getBoolean("Clase"))==true){
+                        String LEntrada=currentGroup.getJSONObject("Lunes").getString("Entrada");
+                        String LSalida=currentGroup.getJSONObject("Lunes").getString("Salida");
+                    }
+                    else{
+                        String LEntrada=null;
+                        String LSalida=null;
+                    }
+                    
+                    if(currentGroup.getJSONObject("Martes").getBoolean("Clase")){
+                        String MEntrada=currentGroup.getJSONObject("Martes").getString("Entrada");
+                        String MSalida=currentGroup.getJSONObject("Martes").getString("Salida");
+                    }
+                    else{
+                        String MEntrada=null;
+                        String MSalida=null;
+                    }
+                    
+                    if(currentGroup.getJSONObject("Miercoles").getBoolean("Clase")){
+                        String MiEntrada=currentGroup.getJSONObject("Miercoles").getString("Entrada");
+                        String MiSalida=currentGroup.getJSONObject("Miercoles").getString("Salida");
+                    }
+                    else{
+                        String MiEntrada=null;
+                        String MiSalida=null;
+                    }
+                    
+                    if(currentGroup.getJSONObject("Jueves").getBoolean("Clase")){
+                        String JEntrada=currentGroup.getJSONObject("Jueves").getString("Entrada");
+                    }
+                }
+            }
+            
+            
+        }
+        catch(SQLException sqlEx)
+        {
+                rtnStmt.put("Registro", false);
+                rtnStmt.put("Mensaje","SQLException");
+                return rtnStmt.toString();
+        }
        return "{'Nada':0}";
        
    }
